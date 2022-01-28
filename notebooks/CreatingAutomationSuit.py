c4513d0bb8ef2593507510e4f994c4f158aea8ab
Sanjog Maheshwari <sanjog.maheshwari@yahoo.com>
2022-01-28 16:15:49 +0530

    Specific modules can be added

--- notebooks/CreatingAutomationSuit.py
+++ notebooks/CreatingAutomationSuit.py
@@ -3,13 +3,19 @@ import xlsxwriter
 import ctypes  # An included library with Python install.
 
 def Mbox(text, title='Smart Test', style=0):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    # return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    return 1
 
 ## SmarTest using test Prioritization Method
 ## Reading The Data file
 Mbox("The master file is ready for execution..!")
 # dataset = pd.read_excel('SampleTestData.xlsx')
 dataset = pd.read_excel('TestData _MasterInput_0.3.xlsx')
+
+modules = input("Insert Selected Modules or leave it blank : ") #['TNT', 'CFE']
+modules = modules.split(sep=',')
+if modules != '':
+    dataset = dataset[dataset.Module.isin(modules)]
 
 ## Extracting The release ID
 unique_release = dataset['Release ID'].unique()
@@ -29,6 +35,7 @@ n_defects = dataset['Linked Defects']
 e_pron_test = dataset['Error Prone Test case']
 s_defects = dataset['Severity of Defects']
 automation_script_name = dataset['Automation script']
+module = dataset['Module']
 # print(release)
 
 ## Selecting latest release=7.1
@@ -52,8 +59,8 @@ if number_test_cases_current_release == 0:
 
 ##Processing of data is based on the test case priority, linked defects and error prone nature of the test cases
 #Mbox("Taking the combinations for the input paramenters such as test priority, severity, defect linked, error pron")
-for t_id,tc_title,rel,t_pri, n_def, e_p_test,s_def,a_script_name in \
-        zip(test_id,test_case_title,release,t_priority, n_defects, e_pron_test,s_defects,automation_script_name):
+for t_id,tc_title,rel,t_pri, n_def, e_p_test,s_def,a_script_name,mod in \
+        zip(test_id,test_case_title,release,t_priority, n_defects, e_pron_test,s_defects,automation_script_name,module):
 
 #Process the data the latest(nth) release
 #Generate the prioritsed test case list based on relevant condition.
@@ -61,17 +68,17 @@ for t_id,tc_title,rel,t_pri, n_def, e_p_test,s_def,a_script_name in \
     if rel == current_release:
         count += 1
         if t_pri == 'P0' or t_pri == 'P1' and n_def >= 0 and e_p_test == 'Yes' and s_def in ('High', 'Medium'):
-            current_release_P0_P1.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def))
+            current_release_P0_P1.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def, mod))
         if t_pri == 'P2' or t_pri == 'P3' and n_def >= 2 and e_p_test == 'Yes' and s_def == 'High':
-            current_release_P2_P3.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def))
+            current_release_P2_P3.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def, mod))
 
 ##Process the data for remaining releases and generate the prioritised test case list
 
     if rel != current_release:
         if t_pri == 'P0' or t_pri == 'P1' and n_def >= 0 and e_p_test == 'Yes' and s_def in ('High', 'Medium'):
-            previous_releases_P0_P1.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def))
+            previous_releases_P0_P1.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def, mod))
         if t_pri == 'P2' or t_pri == 'P3' and n_def >= 2 and e_p_test == 'Yes' and s_def == 'High':
-            previous_releases_P2_P3.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def))
+            previous_releases_P2_P3.append((t_id, tc_title, a_script_name, t_pri, n_def, s_def, mod))
 
 ## Displaying the various test case count in each section.
 
@@ -129,53 +136,50 @@ all_tc = []
 all_info_tc = []
 for i in range(len(master_dataframe[1])):
     # if df[2][i] == "Manual Test Case":
-        all_tc.append((master_dataframe[0][i], master_dataframe[1][i], master_dataframe[2][i]))
-        all_info_tc.append((master_dataframe[0][i], master_dataframe[1][i], master_dataframe[2][i],master_dataframe[3][i],master_dataframe[4][i],master_dataframe[5][i]))
+        all_tc.append((master_dataframe[0][i], master_dataframe[1][i], master_dataframe[2][i], master_dataframe[6][i]))
+        all_info_tc.append((master_dataframe[0][i], master_dataframe[1][i], master_dataframe[2][i],
+                            master_dataframe[3][i], master_dataframe[4][i], master_dataframe[5][i],
+                            master_dataframe[6][i]))
 
 limit = int(input("Insert the Limit: "))
 
-if limit ==0:
-    pd.DataFrame(all_tc, columns=["Test ID", "Test case Title", "Automation Script Name"]).to_excel(writer,
+if limit == 0:
+    pd.DataFrame(all_tc, columns=["Test ID", "Test case Title", "Automation Script Name", 'Module']).to_excel(writer,
                                                                                   sheet_name="Master_Output",
                                                                                   index=False)
 else:
     limited_test = pd.DataFrame(all_info_tc, columns=["Test ID", "Test case Title", "Automation Script Name","Test Priority",
-                                               "Linked Defects", "Severity Defects"])
+                                               "Linked Defects", "Severity Defects", 'Module'])
     master = limited_test.sort_values(by=["Test Priority",  "Severity Defects","Linked Defects"], ignore_index=False)
     final = []
     for i in range(len(master["Test ID"])):
-        final.append((master["Test ID"][i], master["Test case Title"][i],master["Automation Script Name"][i]))
-    master_dataframe = pd.DataFrame(final, columns=["Test ID", "Test case Title", "Automation Script Name"])
+        final.append((master["Test ID"][i], master["Test case Title"][i],master["Automation Script Name"][i], master['module'])[i])
+    master_dataframe = pd.DataFrame(final, columns=["Test ID", "Test case Title", "Automation Script Name",'Module'])
     master_dataframe = master_dataframe.head(limit)
     master_dataframe.to_excel(writer, sheet_name="Master_Output",index=False)
-    # master_dataframe.set_index(master_dataframe['index'].head(limit), inplace=True)
-# writer.save()
 
-# Exctracting information of automation scripts name and manual test case names
-# print(df)
-# writer = pd.ExcelWriter(final_output_file, engine='xlsxwriter')
-# final_output = pd.read_excel(final_output_file)
+
 df = master_dataframe
 m_tc = []
 a_tc = []
 if limit == 0:
     for i in range(len(df[0])):
         if df[2][i] == "Manual Testing":
-            m_tc.append((df[0][i], df[1][i]))
+            m_tc.append((df[0][i], df[1][i], df[6][i]))
         else:
-            a_tc.append(df[2][i])
+            a_tc.append((df[2][i], df[6][i]))
 else:
     for i in range(len(df['Test ID'])):
         if df['Automation Script Name'][i] == "Manual Testing":
-            m_tc.append((df['Test ID'][i], df['Test case Title'][i]))
+            m_tc.append((df['Test ID'][i], df['Test case Title'][i], df['Module'][i]))
         else:
-            a_tc.append(df['Automation Script Name'][i])
+            a_tc.append(df['Automation Script Name'][i], df['Module'][i])
 
-df_auto = pd.DataFrame(set(a_tc), columns=["Automation Script Name"])
+df_auto = pd.DataFrame(set(a_tc), columns=["Automation Script Name", "Module"])
 df_auto.index += 1
 df_auto.index.name="S.No."
 df_auto.to_excel(writer, sheet_name="Automation_Scripts")
-df_manual = pd.DataFrame(m_tc, columns=["Test ID", "Manual Test Case"])
+df_manual = pd.DataFrame(m_tc, columns=["Test ID", "Manual Test Case", "Module"])
 df_manual.index += 1
 df_manual.index.name = "S.No."
 df_manual.to_excel(writer, sheet_name="Manual_TC")
